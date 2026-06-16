f4233407f5e975b8936fc1653b42dd0af3965c63
houssem-eddinjallouli <houssem-eddin.jallouli@esprit.tn>
2026-06-16 11:23:57 +0100

    RE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { PdfComponent } from './pdf/pdf.component';
 import { OffersComponent } from './offers/offers.component';
 
 export const routes: Routes = [
-  //{ path: '', redirectTo: '/badiaa', pathMatch: 'full' },
-  { path: '**', component: Error404Component },
+  { path: '', redirectTo: '/badiaa', pathMatch: 'full' },
   { path: 'badiaa', component: HomeComponent },
   { path: 'catalogue', component: CatalogueComponent },
   { path: 'offres', component: OffersComponent },
   { path: 'pdf', component: PdfComponent },
+  { path: '**', component: Error404Component },
 ];
